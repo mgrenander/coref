2fe8971ee9895132c6017e5def0792ce17b22b42
mgrenander <matt.grenander@gmail.com>
2021-02-02 00:48:10 -0500

    convert bert tokens to format suitable for parsing

---
+++ tokenize.py
@@ -0,0 +1,64 @@
+import jsonlines
+import re
+from tqdm import tqdm
+
+
+def convert_bert_word(word):
+    """
+    Convert bert token to regular word. Convert -LRB- and -RRB- tokens to regular parentheses
+    """
+    converted_word = re.sub("##|\[SEP\]|\[CLS\]", "", word)
+    if converted_word == '-LRB-':
+        return '('
+    elif converted_word == '-RRB-':
+        return ')'
+    else:
+        return converted_word
+
+
+def adjust_cluster_indices(clusters, sent_start, sent_end):
+    """
+    Adjust cluster indices to reflect their position within an individual sentence.
+    """
+    adjusted_clusters = []
+    for cluster in clusters:
+        for span in cluster:
+            if span[0] > sent_start and span[1] < sent_end:
+                adjusted_clusters.append((span[0] - sent_start, span[1] - sent_start))
+    return adjusted_clusters
+
+
+if __name__ == "__main__":
+    dataset = 'test'
+    outputs = []
+    with jsonlines.open("data/{}.english.512.jsonlines".format(dataset)) as reader:
+        for line in reader:
+            outputs.append(line)
+
+    mapped_outputs = []  # Will hold the final results: sentences and mapped span indices
+    for output in tqdm(outputs):
+        comb_text = [word for sentence in output['sentences'] for word in sentence]
+        sentence_start_idx = 0
+        sent_so_far = []
+        word_so_far = []
+        sentence_map = output['sentence_map']
+        subtoken_map = output['subtoken_map']
+        clusters = output['clusters']
+        for i, subword in enumerate(comb_text):
+            if i != 0 and sentence_map[i-1] != sentence_map[i]:  # New sentence
+                mapped_outputs.append({'words': sent_so_far, 'spans': adjust_cluster_indices(clusters, sentence_start_idx, i-1)})
+                sent_so_far = []
+                sentence_start_idx = i
+
+            if i != 0 and subtoken_map[i-1] != subtoken_map[i]:  # New word
+                sent_so_far.append(convert_bert_word(''.join(word_so_far)))
+                word_so_far = []
+            else:
+                word_so_far.append(subword)
+
+    with jsonlines.open('data/{}.adjust_span_sents.jsonlines'.format(dataset), mode='w') as w:
+        w.write(mapped_outputs)
+
+    with open('data/{}.raw_tokens.sentences'.format(dataset), mode='w') as f:
+        for output in mapped_outputs:
+            f.write(' '.join(output['words']) + '\n')
