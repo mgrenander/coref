212deed9f5d85ebdf71912d1f1a6d63694b26e83
mgrenander <matt.grenander@gmail.com>
2021-02-04 00:40:52 -0500

    score parser spans

---
+++ score_parser_spans.py
@@ -0,0 +1,33 @@
+import jsonlines
+import sys
+
+
+if __name__ == "__main__":
+    # Load gold spans
+    gold_spans = []
+    with jsonlines.open("data/test.adjust_span_sents.jsonlines") as reader:
+        for line in reader:
+            gold_spans.append(set([tuple(x) for x in line['spans']]))
+
+    # Load pred spans
+    pred_spans = []
+    with open("data/parser_spans_preds/test.{}.preds".format(int(sys.argv[1])), 'r') as f:
+        for line in f:
+            if not line.strip():
+                pred_spans.append(set(eval(line.strip()[5:-1])))
+            else:
+                pred_spans.append(set())
+
+    num_correct = 0
+    num_gold = 0
+    num_pred = 0
+    for gold_span, pred_span in zip(gold_spans, pred_spans):
+        correct_spans = gold_span.intersection(pred_span)
+        num_correct += len(correct_spans)
+        num_gold += len(gold_span)
+        num_pred += len(pred_span)
+
+    precision = num_correct / num_pred
+    recall = num_correct / num_gold
+    f1 = (2 * precision * recall) / (precision + recall) if precision + recall != 0.0 else 0.0
+    print("{}: precision={}, recall={}, f1={}".format(int(sys.argv[1]), precision, recall, f1))
